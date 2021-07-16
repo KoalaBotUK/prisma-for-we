f37385ff49bfe8a3d26aee215344fcbe44fc675c
Jaddison011 <ja3g20@soton.ac.uk>
2021-07-16 13:46:43 +0100

    Written some sqlite queries to read and save some users to the database

--- app.js
+++ app.js
@@ -2,8 +2,44 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 // A `main` function so that you can use async/await
 async function main() {
-    // ... you will write your Prisma Client queries here
-    console.log("you will write your Prisma Client queries here")
+    let user = await prisma.user.create({
+        data: {
+            email: 'bob@gmail.com',
+            name: 'Bob Roberts',
+        },
+    })
+    console.log(user)
+
+    user = await prisma.user.create({
+        data: {
+            name: 'John',
+            email: 'john@hotmail.com'
+        },
+    })
+
+    console.log(user)
+    user = await prisma.user.create({
+        data: {
+            name: 'Smith',
+            email: 'smithy@gmail.com'
+        },
+    })
+    console.log(user)
+
+    user = await prisma.user.create({
+        data: {
+            name: 'Jeff',
+            email: 'jeffrey@jeff.com'
+        },
+    })
+    console.log(user)
+
+    user = await prisma.user.findUnique({
+        where: {
+            email: 'jeffrey@jeff.com',
+        },
+    })
+    console.log(user)
 }
 main()
     .catch(e => {
